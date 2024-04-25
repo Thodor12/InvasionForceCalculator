d8c38a3a2bfd017c8bf491384fd8e6eee73e3f5d
Thom van den Akker <thodor20@gmail.com>
2024-04-25 12:07:02 +0200

    Add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thodor12.github.io',
+  base: 'InvasionForceCalculator',
   integrations: [react(), tailwind()]
 });
